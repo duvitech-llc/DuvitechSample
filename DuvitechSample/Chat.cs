e31caa464f6954f5ae6de2b32ffb8c01fea67728
George Vigelette <gvigelet@duvitech.com>
2017-11-22 13:03:01 -0500

    Add project files.

---
+++ DuvitechSample/Chat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuvitechSample
+{
+    public class Chat : Hub
+    {
+        public async Task Send(string message)
+        {
+            await Clients.All.InvokeAsync("Send", message);
+        }
+
+    }
+}
